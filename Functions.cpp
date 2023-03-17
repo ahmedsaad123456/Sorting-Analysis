23975ec8d9c610415c067088128b19e7882dfa69
Shahd Mostafa <mostafashahd@gmail.com>
2023-03-17 17:40:38 +0200

    Edit in SelectionSort

--- Functions.cpp
+++ Functions.cpp
@@ -132,10 +132,10 @@ namespace sortlib
     //Selection Sort
     template <typename T>
     void selectionSort(T arr[] , int n){
-        for(int i = 0 ; i < n ; i++)
+        for(int i = 0 ; i < n - 1 ; i++)
         {
             int min = i;
-            for(int j = i+1 ; j < n ; j++)
+            for(int j = i+1 ; j < n  ; j++)
             {
                 if(arr[j] < arr[min])
                     min = j ;
