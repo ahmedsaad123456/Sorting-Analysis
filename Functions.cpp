b8d41b982946b6f4b519fe7c51526bb12d97f993
maryamosama33 <maryamosama3383@gmail.com>
2023-03-17 12:13:06 +0200

    Insertion sort and quick sort

--- Functions.cpp
+++ Functions.cpp
@@ -5,6 +5,22 @@ using namespace std;
 
 namespace sortlib 
 {
+    //--------------------------------------------
+    //Insertion sort
+    template<typename T>
+    void Insertion_sort(T arr[],int n){
+        for (int i = 1; i < n; ++i) {
+            T key = arr[i];
+            int j = i-1;
+            while(j>=0 && arr[j]>key){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = key;
+        }
+    }
+    //--------------------------------------------
+    //Bubble sort
     template <typename T>
 
     void BubbleSort( T arr[] , int n){
@@ -17,8 +33,8 @@ namespace sortlib
             }
         }
     }
-
-
+    //--------------------------------------------
+    //Count sort
     template <typename T>
 
     void CountSort(T arr[] , int n){
@@ -50,8 +66,8 @@ namespace sortlib
 
         }
     }
-
-
+    //--------------------------------------------
+    //Shell sort
     template <typename T>
     void ShellSort(T arr[], int n){
         int r=2;
@@ -73,27 +89,58 @@ namespace sortlib
                 j--;
             }    
         }
-
-
+    }
+    //--------------------------------------------
+    //Quick sort
+    template<typename T>
+    int partition (T arr[], int left ,int right){
+        int i = left;
+        int j = right;
+        int Pivot = i;
+        while(true){
+            while(arr[Pivot] <= arr[j] && Pivot!=j){
+                j--;
+            }
+            if(Pivot == j)
+                break;
+            else if(arr[j]<arr[Pivot]){
+                swap(arr[j],arr[Pivot]);
+                Pivot = j;
+            }
+            while(arr[Pivot]>=arr[i] && Pivot !=i){
+                i++;
+            }
+            if(Pivot == i)
+                break;
+            else if(arr[i]>arr[Pivot]){
+                swap(arr[i],arr[Pivot]);
+                Pivot = i;
+            }
+        }
+        return Pivot;
+    }
+    template<typename T>
+    void quick_sort(T arr[],int left,int right){
+        if(left < right){
+            int Pivot = partition(arr , left, right);
+            quick_sort(arr ,left, Pivot-1);
+            quick_sort(arr ,Pivot+1, right);
+        }
     }
 
 
-
-
-
-
+   //--------------------------------------------
+   //Print
     template <typename T>
     void print(T arr[] ,int n){
         for(int i=0 ; i<n ; i++){
             cout<<arr[i]<<" ";
         }
-
         cout<<endl;
-
     }
 
 
-};
+}
 
 
 
@@ -106,12 +153,14 @@ int main(){
     using namespace sortlib;
     int arr[]={ 33 , 31 ,40 , 8 , 12 , 17 ,8, 25 ,42};
     int n = sizeof(arr)/ sizeof(arr[0]);
-    BubbleSort(arr , n);
-    print(arr , n);
+//    BubbleSort(arr , n);
+//    print(arr , n);
     //CountSort(arr , n);
     //print(arr , n);
-    ShellSort(arr, n);
-    print(arr , n);
+//    ShellSort(arr, n);
+//    print(arr , n);
+    quick_sort(arr,0,n-1);
+    print(arr,n);
 
 
 
